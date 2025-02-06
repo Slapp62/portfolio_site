41543e389da610b2b870be6279dba9a90ca66ce2
Simcha <slapp62@gmail.com>
2025-02-06 23:01:11 +0200

    sdf

--- projects/Javascript Projects/World Clock/script.js
+++ projects/Javascript Projects/World Clock/script.js
@@ -1,27 +1,69 @@
 const clockIntervals = new Map();
+const API_KEY = 'fcf85a09e3485e38f72aac87b74b4571'
+
+const  getWeatherData = async (cityName) =>{
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data) 
+    } catch (error){
+        console.error('Fetch: error', error)
+    }
+    ;
+    
+}
 
 document.addEventListener('DOMContentLoaded', () => {
-    const defaultCitiesArr = ['Jerusalem', 'Tokyo', 'London', 'New Delhi', 'New York'];
+    const defaultCitiesArr = ['Jerusalem', 'Tokyo', 'London', 'Singapore', 'New York'];
+    let cities = document.querySelectorAll('.clockForm');
+    //console.log(cities[0].children);
+    
+    for (let i=0; i<defaultCitiesArr.length; i++){
+        const timezone = moment.tz.names().find(timezone => timezone.toLowerCase().includes(defaultCitiesArr[i].toLowerCase()) || timezone.replace('_', ' ').toLowerCase().includes(defaultCitiesArr[i].toLowerCase()));
 
+        const interval_ID = setInterval(() => {
+            cities[i].children[4].innerHTML = moment().tz(timezone).format('HH:mm:ss');
+        }, 1000)
+        
+        clockIntervals.set(cities[i], interval_ID);
+       cities[i].children[3].innerHTML = defaultCitiesArr[i];  
+    }
+    
 })
-
 
 document.querySelectorAll('.clockForm').forEach(clockForm =>{
     clockForm.addEventListener('submit', (e) =>{
         e.preventDefault();
         const cityInput = clockForm.querySelector('.cityInput').value;
         
-        console.log(clockForm);
-        console.log(cityInput);
-        updateClock(clockForm, cityInput);       
+        updateClock(clockForm, cityInput);
+        getWeatherData(cityInput);       
     });
 });
 
 const updateClock = (clockForm, cityInput) =>{
-    const cityName = clockForm.querySelector('h2');
-    cityName.innerHTML = cityInput;
+    const cityName = clockForm.querySelector('p');
+    cityName.innerHTML = cityInput.toUpperCase();
 
-    const timezone = moment.tz.names();
+    const timezone = moment.tz.names().find(timezone => timezone.toLowerCase().includes(cityInput.toLowerCase()) || timezone.replace('_', ' ').toLowerCase().includes(cityInput.toLowerCase()));
+
+    if (!timezone || cityInput === '' || cityInput === null) {
+        //cityName.style.color = "red";
+        cityName.innerHTML = 'City Not Found/Invalid Entry';
+        return;
+    }
+
+    if (clockIntervals.has(clockForm)){
+        clearInterval(clockIntervals.get(clockForm))
+    }
+
+    const interval_ID = setInterval(() => {
+        clockForm.querySelector(".time").innerHTML = moment().tz(timezone).format('HH:mm:ss');
+    }, 1000)
+
+    clockIntervals.set(clockForm, interval_ID);
+
     console.log(timezone);
     
 }
