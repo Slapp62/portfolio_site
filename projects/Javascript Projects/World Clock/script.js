2b891f1f7069c6268666fe07d7503d563e444786
Elazar Lapp <slapp62@gmail.com>
2025-02-06 19:03:57 +0200

    coumtry

--- projects/Javascript Projects/World Clock/script.js
+++ projects/Javascript Projects/World Clock/script.js
@@ -1,57 +1,27 @@
+const clockIntervals = new Map();
 
-setInterval(() => {
-    const time = new Date();
-    const idLink = document.getElementById("jerusalem");
-    idLink.innerHTML = time.toLocaleTimeString();
+document.addEventListener('DOMContentLoaded', () => {
+    const defaultCitiesArr = ['Jerusalem', 'Tokyo', 'London', 'New Delhi', 'New York'];
+
 })
 
-setInterval(() => {
-    const time = new Date();
-    time.setHours(time.getHours() - 7);
-    const idLink = document.getElementById("new_york");
-    idLink.innerHTML = time.toLocaleTimeString();
-})
 
-setInterval(() => {
-    const time = new Date();
-    time.setHours(time.getHours() + 7);
-    const idLink = document.getElementById("tokyo");
-    idLink.innerHTML = time.toLocaleTimeString();
-})
+document.querySelectorAll('.clockForm').forEach(clockForm =>{
+    clockForm.addEventListener('submit', (e) =>{
+        e.preventDefault();
+        const cityInput = clockForm.querySelector('.cityInput').value;
+        
+        console.log(clockForm);
+        console.log(cityInput);
+        updateClock(clockForm, cityInput);       
+    });
+});
 
-setInterval(() => {
-    const time = new Date();
-    time.setHours(time.getHours() - 2);
-    const idLink = document.getElementById("london");
-    idLink.innerHTML = time.toLocaleTimeString();
-})
+const updateClock = (clockForm, cityInput) =>{
+    const cityName = clockForm.querySelector('h2');
+    cityName.innerHTML = cityInput;
 
-setInterval(() => {
-    const time = new Date();
-    time.setHours(time.getHours() + 3.5);
-    time.setMinutes(time.getMinutes() + 30);
-    const idLink = document.getElementById("new_delhi");
-    idLink.innerHTML = time.toLocaleTimeString();
-})
-
-const flagInDiv = (country, divId) =>{
-    const request = new XMLHttpRequest();
-    request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
-    request.send();
-
-    request.onload = () =>{
-        const countryData = JSON.parse(request.response);
-        const flag = countryData[0].flags.png;
-        const selectedDiv = document.getElementById(divId);
-        selectedDiv.style.backgroundImage = `url(${flag})`;
-        selectedDiv.style.backgroundSize = "cover";
-        selectedDiv.style.backgroundPosition = "center";
-    }
+    const timezone = moment.tz.names();
+    console.log(timezone);
     
 }
-
-flagInDiv("USA", "ny");
-flagInDiv("Japan", "jpn");
-flagInDiv("India", "ind");
-flagInDiv("Britain", "lnd");
-flagInDiv("Israel", "jlm");
