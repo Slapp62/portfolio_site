452ad95ed340fdea58767b22f42f2c84e6d742db
Simcha <slapp62@gmail.com>
2025-02-11 00:51:27 +0200

    mikvah calendar beginning

--- projects/Javascript Projects/Mikvah Calendar/main.js
+++ projects/Javascript Projects/Mikvah Calendar/main.js
@@ -1,7 +1,35 @@
+const form = document.querySelector('.form');
+
 document.addEventListener('DOMContentLoaded', function() {
-    const calendarEl = document.getElementById('calendar')
-    const calendar = new FullCalendar.Calendar(calendarEl, {
-      initialView: 'dayGridMonth'
-    })
-    calendar.render()
-  })
+  let calendarEl = document.getElementById('calendar');
+  let calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: 'dayGridMonth'
+  });
+ 
+
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const machzorDate = document.querySelector('#machzorDate').value;
+    const onah = document.querySelector('#onah').value;
+    const machzor = `${machzorDate} ${onah}`;
+    console.log(machzor);
+
+    if (machzorDate) {
+      let onahStart = onah === 'onah1' ? 'T00:00:00' : 'T12:00:00';
+      let onahEnd = onah === 'onah1' ? machzorDate + 'T12:00:00' : machzorDate + 'T23:59:59';
+
+      let dateObj = new Date(machzorDate);
+      dateObj.setDate(dateObj.getDate() + 30);
+      let averageOnah = dateObj.toISOString().split('T')[0];
+
+      calendar.addEvent({
+          title: 'Average Onah',
+          start: averageOnah + onahStart,
+          end: averageOnah + onahEnd
+      });
+    }
+  });
+
+ calendar.render();
+});
