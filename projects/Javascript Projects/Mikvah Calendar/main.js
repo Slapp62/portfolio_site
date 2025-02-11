82c352c7042d7c9ab4f4044eba69951a88d1a743
Simcha <slapp62@gmail.com>
2025-02-11 23:42:12 +0200

    day onah function

--- projects/Javascript Projects/Mikvah Calendar/main.js
+++ projects/Javascript Projects/Mikvah Calendar/main.js
@@ -1,35 +1,69 @@
 const form = document.querySelector('.form');
+let calendar;
 
-document.addEventListener('DOMContentLoaded', function() {
+const createCalendar = () =>{
   let calendarEl = document.getElementById('calendar');
-  let calendar = new FullCalendar.Calendar(calendarEl, {
-    initialView: 'dayGridMonth'
+  calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: 'dayGridMonth',
+    selectable: true,
+    editable: true,
   });
+  calendar.render();
+}
+
+const averageOnah = (machzorDate, onahStart, onahEnd)=>{
+  let dateObj = new Date(machzorDate);
+  dateObj.setDate(dateObj.getDate() + 29);
+  let averageOnah = dateObj.toISOString().split('T')[0];
+
+  calendar.addEvent({
+      title: 'Average Onah',
+      start: averageOnah + onahStart,
+      end: averageOnah + onahEnd,
+  });
+}
+
+const dayOnah = (machzorDate, onahStart, onahEnd) =>{
+  let dateObj = new Date(machzorDate);
+  dateObj.setMonth(dateObj.getMonth() + 1);
+  let dayOnah = dateObj.toISOString().split('T')[0];   
+  console.log(dayOnah);
+  
+
+  calendar.addEvent({
+    title: 'Day Onah',
+    start: dayOnah + onahStart,
+    end: dayOnah + onahEnd,
+  });
+}
+
+document.addEventListener('DOMContentLoaded', () =>{
+  createCalendar();
  
-
-
   form.addEventListener('submit', (e) => {
     e.preventDefault();
     const machzorDate = document.querySelector('#machzorDate').value;
     const onah = document.querySelector('#onah').value;
     const machzor = `${machzorDate} ${onah}`;
-    console.log(machzor);
+    console.log('got period: ' + machzor);
+
+    if (!machzorDate){
+      alert('please choose a date');
+      return
+    }
 
     if (machzorDate) {
-      let onahStart = onah === 'onah1' ? 'T00:00:00' : 'T12:00:00';
-      let onahEnd = onah === 'onah1' ? machzorDate + 'T12:00:00' : machzorDate + 'T23:59:59';
+      if (onah === ''){
+        alert('please choose an onah');
+        return
+      }
 
-      let dateObj = new Date(machzorDate);
-      dateObj.setDate(dateObj.getDate() + 30);
-      let averageOnah = dateObj.toISOString().split('T')[0];
-
-      calendar.addEvent({
-          title: 'Average Onah',
-          start: averageOnah + onahStart,
-          end: averageOnah + onahEnd
-      });
+      let onahStart = onah === 'onahAM' ? 'T00:00:00' : 'T12:00:00';
+      let onahEnd = onah === 'onahPM' ? 'T23:59:59' :  'T12:00:00';
+            
+      averageOnah(machzorDate, onahStart, onahEnd);
+      dayOnah(machzorDate, onahStart, onahEnd);
+      
     }
   });
-
- calendar.render();
 });
