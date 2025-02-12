06ae10969b2f11a96c7b01bb5659941a56bb62f2
Simcha <slapp62@gmail.com>
2025-02-12 02:48:20 +0200

    class for storage

--- projects/Javascript Projects/Mikvah Calendar/main.js
+++ projects/Javascript Projects/Mikvah Calendar/main.js
@@ -1,12 +1,60 @@
 const form = document.querySelector('.form');
 let calendar;
 
+class Event {
+  static eventObj = {};
+  static id = 0;
+
+  constructor(title, start, end){
+      this.id = id++;
+      this.title = title;
+      this.start = start;
+      this.end = end;
+
+      Event.eventObj[this.id] = this;
+      localStorage.setItem('events', this)
+    };
+  
+  toFullCalEvent(){
+    return {
+      title: this.title,
+      start: this.start,
+      end: this.end
+    };
+  }
+}
+
 const createCalendar = () =>{
   let calendarEl = document.getElementById('calendar');
   calendar = new FullCalendar.Calendar(calendarEl, {
     initialView: 'dayGridMonth',
     selectable: true,
     editable: true,
+    // dayCellDidMount: async function(info) {
+    //   const gregorianDate = info.date.toISOString().split('T')[0]; // Format YYYY-MM-DD
+
+    //   // Fetch Hebrew date from Hebcal API
+    //   let response = await fetch(`https://www.hebcal.com/converter?cfg=json&gy=${gregorianDate.split('-')[0]}&gm=${gregorianDate.split('-')[1]}&gd=${gregorianDate.split('-')[2]}&g2h=1`);
+    //   let data = await response.json();
+
+    //   // Extract Hebrew day, month, and year in English
+    //   let hebrewDay = data.hd;  // Hebrew day number
+    //   let hebrewMonth = data.hm; // Hebrew month in English
+    //   let hebrewYear = data.hy;  // Hebrew year
+
+    //   // Format the Hebrew date in English
+    //   let hebrewDateEnglish = `${hebrewDay} ${hebrewMonth} ${hebrewYear}`;
+    //   // Get Hebrew date and insert it into the calendar
+    //   let hebrewDate = data.hebrew;
+    //   let hebrewDateDiv = document.createElement('div');
+    //   hebrewDateDiv.style.fontSize = '14px';
+    //   hebrewDateDiv.style.color = 'black';
+    //   hebrewDateDiv.style.textAlign = 'start';
+    //   hebrewDateDiv.style.padding = '5px'
+    //   hebrewDateDiv.innerText = hebrewDateEnglish;
+
+    //   info.el.appendChild(hebrewDateDiv);
+    // },
   });
   calendar.render();
 }
