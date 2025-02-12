8e7e2463d479fd183737cd43b14fcb9f74cb09ae
Elazar Lapp <slapp62@gmail.com>
2025-02-12 22:17:03 +0200

    project view selction

---
+++ Scripts/main.js
@@ -0,0 +1,19 @@
+const htmlSection = document.getElementById('htmlProjects');
+const jsSection = document.getElementById('jsProjects');
+
+const htmlButton = document.getElementById('htmlProjectsBtn');
+const jsButton = document.getElementById('jsProjectsBtn');
+
+htmlSection.style.display = 'flex';
+jsSection.style.display = 'none';
+
+htmlButton.addEventListener('click', () =>{
+    htmlSection.style.display = 'flex';
+    jsSection.style.display = 'none';
+})
+
+jsButton.addEventListener('click', () =>{
+    htmlSection.style.display = 'none';
+    jsSection.style.display = 'flex';
+})
+
