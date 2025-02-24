0d65b1ac1e063f875ab74ecee4a4b0731334ae06
Simcha <slapp62@gmail.com>
2025-02-23 21:45:33 -0500

    calendar update

--- projects/Javascript Projects/Mikvah Calendar/main.js
+++ projects/Javascript Projects/Mikvah Calendar/main.js
@@ -2,8 +2,8 @@ const form = document.querySelector('.form');
 let calendar;
 
 class Event {
-  static eventArr = [];
-  static id = 0;
+  static id = JSON.parse(localStorage.getItem("IDtracker") ) || 0;
+  static eventArr = JSON.parse(localStorage.getItem(Event.id)) || [];
 
   constructor(title, start, end){
       this.title = title;
@@ -11,16 +11,21 @@ class Event {
       this.end = end;
       
       Event.eventArr.push(this);
-      localStorage.setItem(Event.id, JSON.stringify(Event.eventArr))
-      Event.id++;
+      
     };
-  
+
   toFullCalEvent(){
     return {
       title: this.title,
       start: this.start,
       end: this.end
     };
+  }
+
+  static saveOnahs(){
+    Event.id++;
+    localStorage.setItem("IDtracker", JSON.stringify(Event.id));
+    localStorage.setItem(Event.id, JSON.stringify(Event.eventArr))
   }
 }
 
@@ -59,6 +64,53 @@ const createCalendar = () =>{
   calendar.render();
 }
 
+const periodStart = (machzorDate, onahEnd) => {
+  let dateObj = new Date(machzorDate);
+  let periodDate = dateObj.toISOString().split('T')[0];
+
+  calendar.addEvent({
+    title: 'Period Start',
+    start: periodDate,
+    end: onahEnd,
+  });
+
+  let newPeriodDate = new Event('Period Start', periodDate, onahEnd );
+}
+
+const haflagah = (machzorDate, onahStart, onahEnd) => {
+  const periods = Event.eventArr.filter(event => event.title === "Period Start");
+
+  periods.sort((a,b) => new Date(a.start) - new Date(b.start));
+
+  if (periods.length < 2){
+    alert('Haflagah cannot be calculate until there are two periods');
+    return;
+  }
+
+  const lastPeriod = new Date(periods[periods.length - 2].start);
+  const newPeriod = new Date(machzorDate);
+
+  const diffInMS = newPeriod - lastPeriod;
+  const diffInDays = diffInMS / (1000 * 60 * 60 * 24);
+
+  console.log(lastPeriod);
+  console.log(newPeriod);
+  console.log(diffInDays);
+  
+  let dateObj = new Date(machzorDate);
+  dateObj.setDate(dateObj.getDate() + diffInDays);
+  let haflagah = dateObj.toISOString().split('T')[0];
+
+  calendar.addEvent({
+      title: 'Haflagah',
+      start: haflagah + onahStart,
+      end: haflagah + onahEnd,
+  });
+
+  let newHaflagah = new Event('Haflagah', haflagah + onahStart, haflagah + onahEnd );
+  
+}
+
 const averageOnah = (machzorDate, onahStart, onahEnd)=>{
   let dateObj = new Date(machzorDate);
   dateObj.setDate(dateObj.getDate() + 29);
@@ -70,7 +122,8 @@ const averageOnah = (machzorDate, onahStart, onahEnd)=>{
       end: averageOnah + onahEnd,
   });
 
-  new Event('Average Onah', averageOnah + onahStart, averageOnah + onahEnd ) 
+  let newAverageOnah = new Event('Average Onah', averageOnah + onahStart, averageOnah + onahEnd );
+
 }
 
 const dayOnah = (machzorDate, onahStart, onahEnd) =>{
@@ -86,42 +139,50 @@ const dayOnah = (machzorDate, onahStart, onahEnd) =>{
     end: dayOnah + onahEnd,
   });
 
-  new Event('Day Onah', dayOnah + onahStart, dayOnah + onahEnd ) 
+  let newDayOnah = new Event('Day Onah', dayOnah + onahStart, dayOnah + onahEnd ) 
 }
+
+
 
 document.addEventListener('DOMContentLoaded', () =>{
   createCalendar();
- 
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const machzorDate = document.querySelector('#machzorDate').value;
-    const onah = document.querySelector('#onah').value;
-    const machzor = `${machzorDate} ${onah}`;
-    console.log('got period: ' + machzor);
 
-    if (!machzorDate){
-      alert('please choose a date');
+  for (let event of Event.eventArr){
+    calendar.addEvent(event);
+  }
+
+});
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const machzorDate = document.querySelector('#machzorDate').value;
+  const onah = document.querySelector('#onah').value;
+  const machzor = `${machzorDate} ${onah}`;
+  console.log('got period: ' + machzor);
+
+  if (!machzorDate){
+    alert('please choose a date');
+    return
+  }
+
+  if (machzorDate) {
+    if (onah === ''){
+      alert('please choose an onah');
       return
     }
 
-    if (machzorDate) {
-      if (onah === ''){
-        alert('please choose an onah');
-        return
-      }
-
-      let onahStart = onah === 'onahAM' ? 'T00:00:00' : 'T12:00:00';
-      let onahEnd = onah === 'onahPM' ? 'T23:59:59' :  'T12:00:00';
-            
-      averageOnah(machzorDate, onahStart, onahEnd);
-      dayOnah(machzorDate, onahStart, onahEnd);
-      
-    }
-  });
+    let onahStart = onah === 'onahAM' ? 'T00:00:00' : 'T12:00:00';
+    let onahEnd = onah === 'onahPM' ? 'T23:59:59' :  'T12:00:00';
+    
+    periodStart(machzorDate, onahEnd);
+    averageOnah(machzorDate, onahStart, onahEnd);
+    dayOnah(machzorDate, onahStart, onahEnd);
+    haflagah(machzorDate, onahStart, onahEnd);
+  }
 });
 
 const saveBtn = document.querySelector('#save');
 
 saveBtn.addEventListener('click', () => {
-  
+  Event.saveOnahs();
 });
