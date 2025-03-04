98b9751e825aa46dae1c14e8d19a2a8164b51e86
Simcha <slapp62@gmail.com>
2025-03-04 17:45:28 -0500

    vite calc proj update

--- projects/Javascript Projects/Calculator/vite.config.ts
+++ projects/Javascript Projects/Calculator/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/portfolio_site/', // this is the base path to the root of the repo
+  base: '/portfolio_site/projects/Javascript%20Projects/Calculator/dist/', // Note: spaces encoded as %20
 });
