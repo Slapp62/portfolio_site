160899ddd9a73432db944c379556bb01cd092f1d
Simcha <slapp62@gmail.com>
2025-03-23 00:21:32 -0400

    icon

--- projects/Javascript Projects/Calculator/src/main.ts
+++ projects/Javascript Projects/Calculator/src/main.ts
@@ -26,31 +26,64 @@ buttonsDiv?.addEventListener("click", (e) =>{
       (displayInput as HTMLInputElement).value = currentInput;
 
       if (previousInput !== ''){
-        (displayInput as HTMLInputElement).value = `${previousInput} ${selectedOp} ${currentInput}`;
-      }       
+        (historyDiv as HTMLElement).innerHTML = `${previousInput} ${selectedOp}`;
+        (displayInput as HTMLInputElement).value = `${currentInput}`;
+      } 
+      console.log(currentInput);      
     },
 
     decimal: (_button) => {
       if (currentInput === '0' || currentInput === ''){
-        currentInput = '0.'
+        currentInput = '0.';
+        (displayInput as HTMLInputElement).value = currentInput;
+        return
       }
 
+      if (newEntryFlag === true){
+        currentInput = '0.';
+        (displayInput as HTMLInputElement).value = currentInput;
+        newEntryFlag = false;
+        return
+      }
+
+      if (currentInput.includes('.')){
+        console.log('entry already includes a decimal point');
+        return
+      }
+      
       currentInput += '.';
       (displayInput as HTMLInputElement).value = currentInput;
+      console.log(currentInput);
     },
     
     operator: (button) => {
-      (historyDiv as HTMLElement).innerHTML = '';
       if (currentInput === ''){
         return
       }
-      previousInput = currentInput;
+
       selectedOp = button.innerHTML;
       newEntryFlag = true;
-      (displayInput as HTMLInputElement).value = `${previousInput} ${selectedOp}`;
+      previousInput = currentInput;
+      
+      (historyDiv as HTMLElement).innerHTML = '';
+
+      switch (selectedOp){
+        case 'x^' :
+            result = parseFloat(currentInput) * parseFloat(currentInput);
+            (historyDiv as HTMLElement).innerHTML = `square(${previousInput})`;
+            (displayInput as HTMLInputElement).value = `${result}`;
+            previousInput = String(result);
+            console.log(currentInput);
+            return 
+      }
+      
+      previousInput = currentInput;
+      
+      (historyDiv as HTMLElement).innerHTML = `${previousInput} ${selectedOp}`;
+      (displayInput as HTMLInputElement).value = `${previousInput}`;
+      console.log(currentInput);
     },
 
-    
     equals: (_button) => {
       if (currentInput && previousInput && selectedOp){
         switch (selectedOp){
@@ -71,7 +104,7 @@ buttonsDiv?.addEventListener("click", (e) =>{
             break
           case 'x':
             result = parseFloat(previousInput) * parseFloat(currentInput);
-            break    
+            break  
         }
         
         (historyDiv as HTMLElement).innerHTML = `${previousInput} ${selectedOp} ${currentInput} = `;
@@ -87,15 +120,15 @@ buttonsDiv?.addEventListener("click", (e) =>{
     },
     
     clear: (_button) => {
-      (displayInput as HTMLInputElement).value = '0';
-      currentInput = '';
-    },
-
-    allClear: (_button) =>{
       currentInput = '';
       previousInput = '';
       selectedOp = '';
       (historyDiv as HTMLElement).innerHTML = '';
+      (displayInput as HTMLInputElement).value = '0';
+    },
+
+    clearEntry: (_button) =>{
+      currentInput = '';
       (displayInput as HTMLInputElement).value = '0';
     },
   }  
